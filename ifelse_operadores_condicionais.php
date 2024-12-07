f966092812c6f1f7d9007c01cb04ede490c36aaa
Leonardo Borges <leo.nunes.borges@hotmail.com>
2024-12-07 16:03:41 -0300

    Basico do basico de PHP

---
+++ ifelse_operadores_condicionais.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Curso PHP</title>
+        
+    </head>
+
+    <body>
+        <?php
+
+           $idade = 25;
+
+           if($idade == 25){
+            echo 'condição verdadeira';
+           }else{
+            echo 'condição falsa';
+           }
+           
+        ?> 
+
+    </body>
+</html>
---
+++ variaveis.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Curso PHP</title>
+        
+    </head>
+
+    <body>
+        <?php
+
+        $nome = 'João';
+        $idade = 25;
+        $peso = 90.5;
+        $Fumante = true;
+        ?> 
+
+        <h1> Ficha Cadastradal</h1>
+        <br/>
+        <p>Nome: <?= $nome ?></p>
+        <p>Idade: <?= $idade ?></p>
+        <p>Peso: <?= $peso ?></p>
+        <p>Fumante: <?= $Fumante ?></p>
+    </body>
+</html>
