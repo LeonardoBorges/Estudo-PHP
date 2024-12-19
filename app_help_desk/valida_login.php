c6188c016273ca523a0967a115db561f9d61526d
Leonardo Borges <leo.nunes.borges@hotmail.com>
2024-12-19 17:00:25 -0300

    projeto help desk

---
+++ app_help_desk/valida_login.php
@@ -0,0 +1,44 @@
+<?php 
+    /*
+        print_r($_GET); 
+        echo '<br>';
+
+        echo $_GET['email'];    
+        echo '<br>';
+        echo $_GET['senha'];
+    */
+
+    /*
+        print_r($_POST); 
+        echo '<br>';
+
+        echo $_POST['email'];    
+        echo '<br>';
+        echo $_POST['senha'];
+    */
+
+    //variavel que verifica autenticação
+    $usuario_autenticado = false;
+
+    // usuarios do sistema
+    $usuarios_app = [
+    ['email' => 'adm@teste.com.br', 'senha' => '123456'],
+    ['email' => 'user@teste.com.br', 'senha' => 'abcd'],
+
+    ];
+  
+    foreach($usuarios_app as $user){
+
+        if( $user['email'] === $_POST['email'] && $user['senha'] === $_POST['senha']){
+          $usuario_autenticado = true;
+
+        }
+
+        
+    }
+    if($usuario_autenticado){
+        echo 'Usuário Autenticado';
+    } else {
+        header('Location: index.php?login=erro');
+    }
+?>
